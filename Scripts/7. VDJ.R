9ca7770a5870c8cfa771b75be189fbf24a0944c5
Akshay <ammasakshay@gmail.com>
2025-02-21 16:21:00 -0500

    VDJ - TCR Clonal Pipeline

--- Scripts/7. VDJ.R
+++ Scripts/7. VDJ.R
@@ -1,22 +1,37 @@
 
-#
-library(ggplot2)
-library(scRepertoire)
+#Load Required Libraries
 library(Seurat)
-library(ggraph)
+library(SingleCellExperiment)
 library(scater)
 library(tidyverse)
 library(Matrix)
 library(scales)
 library(cowplot)
 library(RCurl)
+library(hdf5r)
+library(dsb)
 library(data.table)
+library(cowplot)
+library(ggplot2)
+library(gridExtra)
+library(SeuratWrappers)
+library(Azimuth)
+library(ggrepel)
+library(patchwork)
+library(scCustomize)
+library(reticulate)
+library(circlize)
+library(ComplexHeatmap)
+library(readxl)
+library(scRepertoire)
+library(igraph)
+library(Cairo)
 
 ################################################# GLobal Variables ###############################################
 #Set dir
-setwd("C:/Users/axi313/Documents/ECCITE_Seq_Katherine/VDJ")
-load.path <- "C:/Users/axi313/Documents/ECCITE_Seq_Katherine/saved_R_data/"
-in.path <- "C:/Users/axi313/Documents/ECCITE_Seq_Katherine/Raw_Data/"
+setwd("C:/Users/ammas/Documents/ECCITE_Seq_Katherine/VDJ")
+load.path <- "C:/Users/ammas/Documents/ECCITE_Seq_Katherine/saved_R_data/"
+in.path <- "C:/Users/ammas/Documents/ECCITE_Seq_Katherine/Raw_Data/"
 t.path <- "/per_sample_outs/vdj_t/filtered_contig_annotations.csv"
 b.path <- "/per_sample_outs/vdj_b/filtered_contig_annotations.csv"
 
@@ -82,32 +97,185 @@ combined.BCR <- addVariable(combined.BCR, variable.name = 'Condition',
 list.receptors <- c(combined.TCR, combined.BCR)
 
 
-save(combined.TCR,combined.BCR, file = paste0(load.path, "Seuratv5_TCR_BCR_Split.RData"))
+#save(combined.TCR,combined.BCR, file = paste0(load.path, "Seuratv5_TCR_BCR_Split.RData"))
 
 #load(paste0(load.path,"TCR_BCR_Split.RData"))
 
-######################################################## Visualisations #########################################
+################################################### Basic Clonal Visualizations ################################################
 
-t.ch <- clonalHomeostasis(combined.TCR, cloneCall = "strict")
-t.cp <- clonalProportion(combined.TCR, cloneCall = "strict")
+setwd('C:/Users/ammas/Documents/ECCITE_Seq_Katherine/VDJ/TCR/Clonal_Visualizations'
+)
 
-b.ch <- clonalHomeostasis(combined.BCR, cloneCall = "strict")
-b.cp <- clonalProportion(combined.BCR, cloneCall = "strict")
+clonalQuant(combined.TCR, 
+            cloneCall="strict", 
+            chain = "both", 
+            scale = FALSE)
+ggsave('Unique_Clones_Strict_TRAB_raw.png',width=24,height=12)
 
-ggsave('TCR_Clonal_Homeostasis_strict.png', width = 15, dpi = 500, t.ch)
-ggsave('TCR_Clonal_Proportion_strict.png', width = 15, dpi = 500, t.cp)
-ggsave('BCR_Clonal_Homeostasis_strict.png', width = 15, dpi = 500, b.ch)
-ggsave('BCR_Clonal_Proportion_strict.png', width = 15, dpi = 500, b.cp)
+### Clonal Abundance
+clonalAbundance(combined.TCR, 
+                cloneCall = "strict", 
+                scale = FALSE)
+ggsave('Clonal_Abundance_TRAB_raw.png',width=16,height=12)
+
+### Clonal Length
+clonalLength(combined.TCR, 
+             cloneCall="aa", 
+             chain = "both") 
+ggsave('Clonal_Length_TRAB_raw.png',width=16,height=12)
+
+clonalHomeostasis(combined.TCR, 
+                  cloneCall = "strict",
+                  cloneSize = c(Rare = 0.001, Small = 0.01, Medium = 0.1, Large = 0.3, Hyperexpanded =
+                                  1))
+ggsave('Clonal_Homeostasis_TRAB_scaled.png',width=24,height=12)
+
+
+######################################## CD3 Composition ##################################
+setwd('C:/Users/ammas/Documents/ECCITE_Seq_Katherine/VDJ/TCR/CD3_Composition')
+
+percentAA(combined.TCR, 
+          chain = "TRA", 
+          aa.length = 20)
+ggsave('Percent_AA_TRA.png',width=26,height=24)
+
+positionalEntropy(combined.TCR, 
+                  chain = "both", 
+                  aa.length = 20)
+ggsave('Positional_Entropy_TRAB.png',width=20,height=15)
+
+vizGenes(combined.TCR, 
+         x.axis = "TRAV",
+         y.axis = NULL,
+         plot = "heatmap",  
+         scale = TRUE)
+ggsave('Heatmap_TRA_V_gene.png',width=12,height=9)
+
+vizGenes(combined.TCR, 
+         x.axis = "TRBV",
+         y.axis = NULL,
+         plot = "heatmap",  
+         scale = TRUE)
+ggsave('Heatmap_TRB_V_gene.png',width=12,height=9)
+
+vizGenes(combined.TCR, 
+         x.axis = "TRAJ",
+         y.axis = NULL,
+         plot = "heatmap",  
+         scale = TRUE)
+ggsave('Heatmap_TRA_J_gene.png',width=12,height=9)
+
+vizGenes(combined.TCR, 
+         x.axis = "TRBJ",
+         y.axis = NULL,
+         plot = "heatmap",  
+         scale = TRUE)
+ggsave('Heatmap_TRB_J_gene.png',width=12,height=9)
+
+percentKmer(combined.TCR, 
+            cloneCall = "aa",
+            chain = "TRA", 
+            motif.length = 3, 
+            top.motifs = 25)
+ggsave('Heatmap_TRA_kmer.png',width=12,height=9)
+
+percentKmer(combined.TCR, 
+            cloneCall = "aa",
+            chain = "TRB", 
+            motif.length = 3, 
+            top.motifs = 25)
+ggsave('Heatmap_TRB_kmer.png',width=12,height=9)
+
+############################### Clonal Diversity and Overlap #####################################################
+setwd('C:/Users/ammas/Documents/ECCITE_Seq_Katherine/VDJ/TCR/Clonal_Diversity')
+
+
+clonalDiversity(combined.TCR, 
+                cloneCall = "strict")
+ggsave('Clonal_Diversity_Strict.png',width=12,height=9)
+
+clonalOverlap(combined.TCR, 
+              cloneCall = "strict", 
+              method = "raw")
+ggsave('Clonal_Overlap_Strict_raw.png',width=24,height=11)
+
+clonalOverlap(combined.TCR, 
+              cloneCall = "gene", 
+              method = "raw")
+ggsave('Clonal_Overlap_Gene_raw.png',width=24,height=11)
+
+
+#### 
+setwd('C:/Users/ammas/Documents/ECCITE_Seq_Katherine/VDJ/TCR/Clonal_Diversity/By_Sample')
+
+clonalCompare(combined.TCR, 
+              top.clones = 20, 
+              samples = c("P1042MED", "P1042CD3","P1042HIV"), 
+              order.by = c("P1042MED", "P1042CD3","P1042HIV"),
+              cloneCall="strict", 
+              relabel.clones = T,
+              graph = "alluvial")
+ggsave('Clonal_Comparison_P1042.png',width=15,height=11)
+
+x <- clonalCompare(combined.TCR, 
+              top.clones = 20, 
+              samples = c("P1042MED", "P1042CD3","P1042HIV"), 
+              order.by = c("P1042MED", "P1042CD3","P1042HIV"),
+              cloneCall="strict", 
+              relabel.clones = T,
+              graph = "alluvial",
+              exportTable = T)
+write.csv(x,'Clonal_Comparison_P1042.csv',row.names = F)
+
+
+clonalCompare(combined.TCR, 
+              top.clones = 20, 
+              samples = c("P1043MED", "P1043CD3","P1043HIV"), 
+              order.by = c("P1043MED", "P1043CD3","P1043HIV"),
+              cloneCall="strict", 
+              relabel.clones = T,
+              graph = "alluvial")
+ggsave('Clonal_Comparison_P1043.png',width=15,height=11)
+
+x <- clonalCompare(combined.TCR, 
+                   top.clones = 20, 
+                   samples = c("P1043MED", "P1043CD3","P1043HIV"), 
+                   order.by = c("P1043MED", "P1043CD3","P1043HIV"),
+                   cloneCall="strict", 
+                   relabel.clones = T,
+                   graph = "alluvial",
+                   exportTable = T)
+write.csv(x,'Clonal_Comparison_P1043.csv',row.names = F)
+
+clonalCompare(combined.TCR, 
+              top.clones = 20, 
+              samples = c("JLKMED", "JLKCD3","JLKHIV"), 
+              order.by = c("JLKMED", "JLKCD3","JLKHIV"),
+              cloneCall="strict", 
+              relabel.clones = T,
+              graph = "alluvial")
+ggsave('Clonal_Comparison_JLK.png',width=15,height=11)
+
+x <- clonalCompare(combined.TCR, 
+                   top.clones = 20, 
+                   samples = c("JLKMED", "JLKCD3","JLKHIV"), 
+                   order.by = c("JLKMED", "JLKCD3","JLKHIV"),
+                   cloneCall="strict", 
+                   relabel.clones = T,
+                   graph = "alluvial",
+                   exportTable = T)
+write.csv(x,'Clonal_Comparison_JLK.csv',row.names = F)
 
 
 ########################################## Merge Seurat #############################################################
 load(paste0(load.path,"Seuratv5_WNN_Complete.RData"))
 
+# TARA All
 # Access the cell barcodes (Assuming they are in the column names of the data slot)
 barcodes <- rownames(seurat_isotype[[]])
 
-# Use gsub to modify the barcodes, removing everything before and including the fourth '_'
-modified_barcodes <- gsub(".*_.*_.*_.*_(.*)", "\\1", barcodes)
+# Use gsub to modify the barcodes, removing everything before and including the third '_'
+modified_barcodes <- gsub(".*_.*_.*_(.*)", "\\1", barcodes)
 modified_barcodes <- paste0(seurat_isotype$orig.ident, "_", modified_barcodes)
 
 # Assign the modified barcodes back to the Seurat object
@@ -131,115 +299,12 @@ seurat.bcr <- combineExpression(combined.BCR,
                                 proportion = FALSE)
 
 
+##############
 
-######################################################## Hyper Expansion Plots ##################################
+library(viridis)  # Load the viridis package
 
-cols <- c("Hyperexpanded (100 < X <= 500)"="#F0F921","Large (20 < X <= 100)" = "#F69441",
-          "Medium (5 < X <= 20)" = "#CA4778" ,"Small (1 < X <= 5)" = "#7D06A5",
-          "Single (0 < X <= 1)" = "#0D0887")
+DimPlot_scCustom(seurat.tcr,split.by = 'orig.ident', reduction = 'wnn.umap', group.by = "cloneSize", split_seurat = T, num_col=3) +
+  scale_color_viridis(option = "viridis", discrete = TRUE, na.value = "gray")
 
-## first ordering the Clone Size as a factor, this prevents the coloring from being in alphabetical order. 
-slot(seurat.tcr, "meta.data")$cloneSize <- factor(slot(seurat.tcr, "meta.data")$cloneSize, 
-                                                  levels = c("Hyperexpanded (100 < X <= 500)", 
-                                                             "Large (20 < X <= 100)", 
-                                                             "Medium (5 < X <= 20)", 
-                                                             "Small (1 < X <= 5)", 
-                                                             "Single (0 < X <= 1)", NA))
-slot(seurat.bcr, "meta.data")$cloneSize <- factor(slot(seurat.bcr, "meta.data")$cloneSize, 
-                                                  levels = c("Hyperexpanded (100 < X <= 500)", 
-                                                             "Large (20 < X <= 100)", 
-                                                             "Medium (5 < X <= 20)", 
-                                                             "Small (1 < X <= 5)", 
-                                                             "Single (0 < X <= 1)", NA))
+ggsave('WNN_TCR_Overlay_byPID.png',dpi=500, height = 11, width = 18)
 
-
-### TCR
-setwd("C:/Users/axi313/Documents/ECCITE_Seq_Katherine/VDJ/Seurat_Plots")
-
-tcr.condition <- DimPlot(seurat.tcr, group.by = "cloneSize", split.by = 'condition', reduction = 'wnn.umap') +
-  scale_color_manual(values=cols) + ggtitle('Expanded TCR Clone Size')
-tcr.stim <- DimPlot(seurat.tcr, group.by = "cloneSize", split.by = 'stim', reduction = 'wnn.umap') +
-  scale_color_manual(values=cols) + ggtitle('Expanded TCR Clone Size')
-
-
-ggsave('TCR_Seurat_Condition.png', width = 12, dpi = 500, tcr.condition)
-ggsave('TCR_Seurat_Stim.png', width = 12, dpi = 500, tcr.stim)
-#ggsave('TCR_Seurat_Sample.png', width = 18, dpi = 500, tcr.sample)
-
-
-### BCR
-bcr.condition <- DimPlot(seurat.bcr, group.by = "cloneSize", split.by = 'condition', reduction = 'wnn.umap') +
-  scale_color_manual(values=cols) + ggtitle('Expanded BCR Clone Size')
-bcr.stim <- DimPlot(seurat.bcr, group.by = "cloneSize", split.by = 'stim', reduction = 'wnn.umap') +
-  scale_color_manual(values=cols) + ggtitle('Expanded BCR Clone Size')
-#bcr.sample <- DimPlot(seurat.bcr, group.by = "cloneSize", split.by = 'sample', reduction = 'wnn.umap') +
-#  scale_color_manual(values=cols) + ggtitle('Expanded BCR Clone Size')
-
-ggsave('BCR_Seurat_Condition.png', width = 12, dpi = 500, bcr.condition)
-ggsave('BCR_Seurat_Stim.png', width = 12, dpi = 500, bcr.stim)
-#ggsave('BCR_Seurat_Sample.png', width = 18, dpi = 500, bcr.sample)
-
-### By Sample 
-setwd('C:/Users/axi313/Documents/ECCITE_Seq_Katherine/VDJ/Seurat_Plots/By_Sample')
-
-for (i in f_names) {
-  plot <- DimPlot(subset(seurat.tcr, subset= `orig.ident`== i), group.by = "cloneSize", reduction = 'wnn.umap') +
-    scale_color_manual(values=cols) + ggtitle(paste0(i,' Expanded TCR Clone Size'))
-  ggsave(paste0(i,'_TCR_Seurat.png'),width = 9,dpi=500,plot)
-}
-
-for (i in f_names) {
-  plot <- DimPlot(subset(seurat.bcr, subset= `orig.ident`== i), group.by = "cloneSize", reduction = 'wnn.umap') +
-    scale_color_manual(values=cols) + ggtitle(paste0(i,' Expanded BCR Clone Size'))
-  ggsave(paste0(i,'_BCR_Seurat.png'),width = 9,dpi=500,plot)
-}
-
-
-############################ Extract output table of most clonal expanded Clone Sizes#############################
-
-### TCR
-setwd('C:/Users/axi313/Documents/ECCITE_Seq_Katherine/VDJ/Clone_Table')
-
-seurat.tcr.aa <- seurat.tcr@meta.data %>%
-  group_by(CTaa, predicted.celltype.l2, orig.ident) %>%
-  count() %>%
-  na.omit()
-
-seurat.tcr.gene <- seurat.tcr@meta.data %>%
-  group_by(CTgene, predicted.celltype.l2, orig.ident) %>%
-  count() %>%
-  na.omit()
-
-seurat.tcr.strict <- seurat.tcr@meta.data %>%
-  group_by(CTstrict, predicted.celltype.l2, orig.ident) %>%
-  count() %>%
-  na.omit()
-
-
-write.csv(seurat.tcr.aa, file = 'TCR_Ranked_Clonal_Expansion_aa_Azimuth.csv', row.names = F)
-write.csv(seurat.tcr.gene, file = 'TCR_Ranked_Clonal_Expansion_gene_Azimuth.csv', row.names = F)
-write.csv(seurat.tcr.strict, file = 'TCR_Ranked_Clonal_Expansion_strict_Azimuth.csv', row.names = F)
-
-### BCR
-
-seurat.bcr.aa <- seurat.bcr@meta.data %>%
-  group_by(CTaa, predicted.celltype.l2, orig.ident) %>%
-  count() %>%
-  na.omit()
-
-seurat.bcr.gene <- seurat.bcr@meta.data %>%
-  group_by(CTgene, predicted.celltype.l2, orig.ident) %>%
-  count() %>%
-  na.omit()
-
-seurat.bcr.strict <- seurat.bcr@meta.data %>%
-  group_by(CTstrict, predicted.celltype.l2, orig.ident) %>%
-  count() %>%
-  na.omit()
-
-
-write.csv(seurat.bcr.aa, file = 'BCR_Ranked_Clonal_Expansion_aa_Azimuth.csv', row.names = F)
-write.csv(seurat.bcr.gene, file = 'BCR_Ranked_Clonal_Expansion_gene_Azimuth.csv', row.names = F)
-write.csv(seurat.bcr.strict, file = 'BCR_Ranked_Clonal_Expansion_strict_Azimuth.csv', row.names = F)
-
-save(seurat.tcr,seurat.bcr, file = paste0(load.path, "Seuratv5_TCR_BCR_VDJ.RData"))
